48929ebb9732c540946010e0ad089ec2d00761c7
Liseth Gutierrez <liliseth12@gmail.com>
2019-01-28 08:07:35 -0400

    Primos pasos para el crud

---
+++ resources/views/app.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        
+    </head>
+    <body>
+        <div id="main" class="container">
+          
+          @yield('content')
+
+        </div>
+      <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
